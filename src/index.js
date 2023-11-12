6dde10aa34950116fc12e7460f303f44af6a33c0
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2023-11-12 18:09:36 +0530

    utils and express setup complete

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ dotenv.config({
 })
 
 connectToDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERROR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!",err);
+})
