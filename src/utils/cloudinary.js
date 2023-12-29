287d655699231dc98d03cd7ac64a59c6ddbd0dc3
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2023-12-29 23:30:27 +0530

    setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as Cloudinary } from "cloudinary";
+import fs from "fs";
+
+Cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Cloudinary.v2.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await Cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file has been uploaded successfully
+    console.log("file successfully uploaded on cloudinary!", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export {uploadOnCloudinary}
